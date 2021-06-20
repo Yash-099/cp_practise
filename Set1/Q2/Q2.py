db9992b6e31f1c331c402d65aeadd180451a432f
Yash-099 <yashbhavsar1205@gmail.com>
2021-06-20 18:21:38 +0530

    Q2 modified

--- Set1/Q2/Q2.py
+++ Set1/Q2/Q2.py
@@ -5,19 +5,39 @@ import numpy as np
 
 
 if __name__ == '__main__':
-	n = 96234
+	n = 10000000
+	listt = []
+	listt.append('')
+	listt.append('1 ')
+	listt.append('1 2 ')
 	D = np.zeros(n+1)
-	D[1] = 1
-	D[2] = 2
+	D[1] = 0
+	D[2] = 1
 	
 	for i in range(3,n+1):
-		if (i+1) % 2 == 0 and (i+1)%3==0:
+		if (i) % 2 == 0 and (i)%3==0:
 			D[i] = min(min(D[int(i/2)],D[int(i/3)]),D[i-1]) + 1
-		elif (i+1) % 2 == 0:
+			if D[i]==D[int(i/2)]+1:
+				listt.append(listt[int(i/2)]+str(i) + ' ')
+			elif D[i]==D[int(i/3)]+1:
+				listt.append(listt[int(i/3)]+str(i) + ' ')
+			elif D[i]==D[i-1]+1:
+				listt.append(listt[i-1]+str(i) + ' ')
+		elif (i) % 2 == 0:
 			D[i] = min(D[int(i/2)],D[i-1]) + 1
-		elif (i+1) % 3 == 0:
+			if D[i]==D[int(i/2)]+1:
+				listt.append(listt[int(i/2)]+str(i) + ' ')
+			elif D[i]==D[i-1]+1:
+				listt.append(listt[i-1]+str(i) + ' ')
+		elif (i) % 3 == 0:
 			D[i] = min(D[int(i/3)],D[i-1]) + 1
+			if D[i]==D[int(i/3)]+1:
+				listt.append(listt[int(i/3)]+str(i) + ' ')
+			elif D[i]==D[i-1]+1:
+				listt.append(listt[i-1]+str(i) + ' ')
 		else :
 			D[i] = D[i-1] + 1
+			listt.append(listt[i-1]+str(i) + ' ')
 
-	print(int(D[n-1]))
+	print(int(D[n]))
+	print(listt[len(listt)-1])
